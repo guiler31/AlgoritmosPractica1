649741023adf37249646119480521232e9c05107
Jorge López <jorge.lopez@live.u-tad.com>
2019-03-03 16:20:33 +0100

    control errores filas y columnas

--- Backtracking/Backtracking.cpp
+++ Backtracking/Backtracking.cpp
@@ -4,11 +4,41 @@
 #include <cstdlib>
 
 using namespace std;
+int filas, columnas;
 
 int main() {
 
-	cout << "¿Numero de filas?" << endl;
-	cout << "¿Numero de columnas?" << endl;
+	cout << "Numero de filas?" << endl;
+	cin >> filas;
+
+	while (!cin) {
+		cout << "Por favor introduzca un numero" << endl;
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> filas;
+	}
+	while (filas < 0) {
+		cout << "Por favor introduzca un numero positivo" << endl;
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> filas;
+	}
+
+	cout << "Numero de columnas?" << endl;
+	cin >> columnas;
+
+	while (!cin) {
+		cout << "Por favor introduzca un numero" << endl;
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> columnas;
+	}
+	while (filas < 0) {
+		cout << "Por favor introduzca un numero positivo" << endl;
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> columnas;
+	}
 
 	cout << "¿Indices de la celda bloqueada (para salir, introducir 0 0)?" << endl;
 	cout << "¿Celda inicial?" << endl;
@@ -17,4 +47,5 @@ int main() {
 
 }
 
-	
+
+
