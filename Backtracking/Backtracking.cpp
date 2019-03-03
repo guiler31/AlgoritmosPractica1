890da7291902fe24769f9f2bc5147a154b103f09
Jorge López <jorge.lopez@live.u-tad.com>
2019-03-03 16:02:42 +0100

    pedir datos

--- Backtracking/Backtracking.cpp
+++ Backtracking/Backtracking.cpp
@@ -7,6 +7,13 @@ using namespace std;
 
 int main() {
 
+	cout << "¿Numero de filas?" << endl;
+	cout << "¿Numero de columnas?" << endl;
+
+	cout << "¿Indices de la celda bloqueada (para salir, introducir 0 0)?" << endl;
+	cout << "¿Celda inicial?" << endl;
+	cout << "¿Celda final?" << endl;
+
 
 }
 
