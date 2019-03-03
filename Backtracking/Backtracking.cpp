2805bf33ed8023b855df63a0505839c494f00cad
Jorge López <jorge.lopez@live.u-tad.com>
2019-03-03 16:26:02 +0100

    while bien hecho

--- Backtracking/Backtracking.cpp
+++ Backtracking/Backtracking.cpp
@@ -11,34 +11,24 @@ int main() {
 	cout << "Numero de filas?" << endl;
 	cin >> filas;
 
-	while (!cin) {
+	while (!cin || filas < 0) {
 		cout << "Por favor introduzca un numero" << endl;
 		cin.clear();
 		cin.ignore(256, '\n');
 		cin >> filas;
 	}
-	while (filas < 0) {
-		cout << "Por favor introduzca un numero positivo" << endl;
-		cin.clear();
-		cin.ignore(256, '\n');
-		cin >> filas;
-	}
+	
 
 	cout << "Numero de columnas?" << endl;
 	cin >> columnas;
 
-	while (!cin) {
+	while (!cin || columnas < 0) {
 		cout << "Por favor introduzca un numero" << endl;
 		cin.clear();
 		cin.ignore(256, '\n');
 		cin >> columnas;
 	}
-	while (filas < 0) {
-		cout << "Por favor introduzca un numero positivo" << endl;
-		cin.clear();
-		cin.ignore(256, '\n');
-		cin >> columnas;
-	}
+	
 
 	cout << "¿Indices de la celda bloqueada (para salir, introducir 0 0)?" << endl;
 	cout << "¿Celda inicial?" << endl;
